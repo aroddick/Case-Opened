6b75d6b32bf30f5eba3b064151e3cd41d9162479
Alan Roddick <aroddick16@gmail.com>
2020-03-22 18:50:03 -0700

    Updated About Me with pictures and some text

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import Daniel from '../assets/daniel.jpeg';
+import Alan from '../assets/alan.jpg'
 
 class AboutUs extends Component {
   state = {};
@@ -6,8 +8,35 @@ class AboutUs extends Component {
     return (
       <div className="container">
         <h1>About Us</h1>
-        <h2>Daniel - Founder</h2>
-        <h2>Alan - Developer</h2>
+        <div style={{width:1000, float:'left'}}>
+          <img
+            src={Daniel}
+            hspace="15"
+            width="200"
+            alt=""
+            style = {{
+              float:'left',
+            }}
+          />
+          <h2>Daniel - Founder</h2>
+          <p>Daniel loves to annoy people and harrass everyone inside this house. 
+          </p>
+        </div>
+        <div style={{width:1000, float:'left'}}>
+          <img
+            src={Alan}
+            hspace="15"
+            width="200"
+            alt=""
+            style = {{
+              float:'left',
+            }}
+          />
+          <h2>Alan - Developer</h2>
+          <p>Alan is the best and never does anything wrong and is loved by all. 
+          </p>
+        </div>
+        
       </div>
     );
   }
